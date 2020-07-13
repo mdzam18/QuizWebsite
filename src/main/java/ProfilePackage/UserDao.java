d492849888fcf70dd0a5382016b5da7611720be3
mdzam18 <mdzam18@freeuni.edu.ge>
2020-07-13 22:19:00 +0400

    add UserDao interface

--- src/main/java/ProfilePackage/UserDao.java
+++ src/main/java/ProfilePackage/UserDao.java
@@ -1,4 +1,43 @@
 package ProfilePackage;
 
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.List;
+
 public interface UserDao {
+    User addUser(String userName, String password) throws SQLException;
+
+    User getUser(int userId) throws SQLException;
+
+    // boolean deleteUser(User user) throws SQLException;
+
+    boolean isCorrectPassword(String userName, String password) throws SQLException;
+
+    List<User> getAllUsers() throws SQLException;
+
+    List<User> getFriends(User user) throws SQLException;
+
+    //  boolean sendFriendRequest(MailSqlDao mDao) throws SQLException;
+
+    //  boolean confirmFriendRequest(MailSqlDao mDao) throws SQLException;
+
+    boolean deleteFriend(User from, User to) throws SQLException;
+
+    List<User> getSentRequests(User user) throws SQLException;
+
+    List<User> getReceivedRequests(User user) throws SQLException;
+
+    boolean createUserTable() throws SQLException, ClassNotFoundException;
+
+    boolean createFriendsTable() throws SQLException, ClassNotFoundException;
+
+    boolean dropTable(String tableName) throws SQLException;
+
+    boolean addProfile(int userId, String name, String surname, Date birthDate, String birthPlace, String status) throws SQLException;
+
+    String hexToString(byte[] bytes);
+
+    String findHashCode(String str);
+
+    String getSalt(int userId) throws SQLException;
 }

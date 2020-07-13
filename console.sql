e9cb9d892a38b7353195f0fb62507d84d7c73a75
nroga18 <nroga18@freeuni.edu.ge>
2020-07-13 18:57:43 +0400

    add sql file

---
+++ console.sql
@@ -0,0 +1,73 @@
+CREATE TABLE Users(
+                       UserId int primary key ,
+                       UserName varchar(255),
+                       Password varchar(255),
+                       IsAdministrator boolean,
+                       Salt varchar(255),
+                       Name varchar(255),
+                       Surname varchar(255),
+                       Birth_Date Date,
+                       Birth_Place varchar(255),
+                       Status varchar(255)
+);
+CREATE TABLE Friends(
+                    SenderId int ,
+                    ReceiverId int ,
+                    Confirmed boolean,
+                    DateSent Date,
+                    foreign key (SenderId) references Users(UserId),
+                    foreign key (ReceiverId) references Users(UserId)
+);
+CREATE TABLE Quiz(
+                      QuizId int primary key,
+                      IsRandom boolean,
+                      IsOnePage boolean,
+                      IsImmediate boolean,
+                      InPracticeMode boolean,
+                      NumberOfQuestions int,
+                      Description varchar(255),
+                      Category varchar(255),
+                      CreatorId int,
+                      foreign key (CreatorId) references Users(UserId)
+);
+CREATE TABLE QuizTag(
+                      QuizId int ,
+                      Tag varchar(255) ,
+                      foreign key (QuizId) references Quiz(QuizId)
+);
+CREATE TABLE Achievements(
+                      UserId int ,
+                      Achievement varchar(255) ,
+                      foreign key (UserId) references Users(UserId)
+);
+CREATE TABLE Questions(
+                      QuestionId int primary key ,
+                      Question varchar(255) ,
+                      Answer varchar(255) ,
+                      Type varchar(255) ,
+                      QuizId int,
+                      foreign key (QuizId) references Quiz(QuizId)
+);
+CREATE TABLE History(
+                      UserId int ,
+                      QuizId int ,
+                      Score int,
+                      Date Date,
+                      Time Time,
+                      foreign key (UserId) references Users(UserId),
+                      foreign key (QuizId) references Quiz(QuizId)
+);
+
+CREATE TABLE Mails(
+                      MailId int primary key ,
+                      SenderId int ,
+                      ReceiverId int ,
+                      Type varchar(255),
+                      Message varchar(255),
+                      Date Date,
+                      Seen int,
+                      foreign key (SenderId) references Users(UserId),
+                      foreign key (ReceiverId) references Users(UserId)
+);
+
+

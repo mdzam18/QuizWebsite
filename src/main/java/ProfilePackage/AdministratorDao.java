4bc7143730c815ea68e2d680cab7927cc0146e4c
agrig18 <agrig18@freeuni.edu.ge>
2020-07-14 15:55:10 +0400

    Uploading Files

--- src/main/java/ProfilePackage/AdministratorDao.java
+++ src/main/java/ProfilePackage/AdministratorDao.java
@@ -1,4 +1,34 @@
 package ProfilePackage;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public interface AdministratorDao {
+	
+	Connection getConnection() throws SQLException;
+	
+	List<User> getAllAdmins() throws SQLException;
+	
+	User addAdmin(String username, String password) throws SQLException;
+	
+	User getAdmin(int userId) throws SQLException;
+	
+	boolean deleteUser(User user) throws SQLException;
+	
+	boolean deleteQuiz(Quiz quiz) throws SQLException;
+	
+	boolean deleteHistory(Quiz quiz) throws SQLException;
+	
+	boolean promoteUser(User user) throws SQLException;
+	
+	boolean createUsersTable() throws SQLException;
+	
+	boolean createHistoryTable() throws SQLException;
+	
+	boolean createQuizTable() throws SQLException;
+	
+	boolean dropTable(String tableName) throws SQLException;
 }
+
+

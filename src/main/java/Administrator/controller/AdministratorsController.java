70de5b65427559b56051fe9ce1582db88e8292e1
agrig18 <agrig18@freeuni.edu.ge>
2020-07-28 21:38:39 +0400

    add controllers

--- src/main/java/Administrator/controller/AdministratorsController.java
+++ src/main/java/Administrator/controller/AdministratorsController.java
@@ -1,2 +1,35 @@
-package Administrator.controller;public class AdministratorsController {
+package Administrator.controller;
+
+import Administrator.dao.AdministratorDao;
+import Administrator.dao.AdministratorSqlDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("admin/admins")
+public class AdministratorsController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = getServletContext();
+        AdministratorDao adminDao = (AdministratorSqlDao) servletContext.getAttribute("adminDao");
+        try {
+            req.setAttribute("users", adminDao.getAllAdmins());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/users.jsp");
+        dispatcher.forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
 }
--- src/main/java/Administrator/controller/QuizzesController.java
+++ src/main/java/Administrator/controller/QuizzesController.java
@@ -1,2 +1,32 @@
-package Administrator.controller;public class QuizzesController {
+package Administrator.controller;
+
+import Administrator.dao.AdministratorDao;
+import Administrator.dao.AdministratorSqlDao;
+import Quiz.QuizDao;
+import Quiz.QuizSqlDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("admin/quizzes")
+public class QuizzesController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = getServletContext();
+        QuizDao quizDao = (QuizSqlDao) servletContext.getAttribute("quizDao");
+//        req.setAttribute("quizzes", quizDao.getAllQuizzes);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/quizzes.jsp");
+        dispatcher.forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
 }

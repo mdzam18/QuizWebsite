a6819d078083993450435f0094718ee371e68649
mdzam18 <56314167+mdzam18@users.noreply.github.com>
2020-07-28 15:25:57 +0400

    Update UserServlet.java

--- src/main/java/UserPackage/UserServlet.java
+++ src/main/java/UserPackage/UserServlet.java
@@ -1,7 +1,6 @@
 package UserPackage;
 
 
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
+import ProfilePackage.FriendsDao;
+import ProfilePackage.FriendsSqlDao;
 import ServletContextPackage.ContextDataNames;
 
 @WebServlet(name = "UserServlet", urlPatterns = {"/UserServlet"})
@@ -20,15 +22,36 @@ public class UserServlet extends HttpServlet {
         ServletContext servletContext = getServletContext();
         UserSqlDao uDao = (UserSqlDao) servletContext.getAttribute(ContextDataNames.USER_DAO);
         String name = httpServletRequest.getParameter("username");
-        String password = httpServletRequest.getParameter("password");
-        String webPageName = "";
-      /*  if (accountManager.containsName(name)) {
-            webPageName = "nameIsAlreadyUsedPage.jsp";
-        } else {
-            Account account = new Account(name, password);
-            accountManager.createAccount(account);
-            webPageName = "welcome.jsp";
-        } */
+        String webPageName = "ProfilePage.jsp";
+        try {
+            if (uDao.containsUserName(name)) {
+                httpServletRequest.getRequestDispatcher(webPageName).forward(httpServletRequest, httpServletResponse);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        ServletContext servletContext = getServletContext();
+        UserSqlDao uDao = (UserSqlDao) servletContext.getAttribute(ContextDataNames.USER_DAO);
+        FriendsDao fDao = null;
+        try {
+            fDao = new FriendsSqlDao();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        String name = httpServletRequest.getParameter("username");
+        String webPageName = "FriendsPage.jsp";
+        try {
+            assert fDao != null;
+            servletContext.setAttribute(ContextDataNames.FRIENDS_DAO, fDao.getFriends(uDao.getUser(uDao.getUserIdByName(name))));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         httpServletRequest.getRequestDispatcher(webPageName).forward(httpServletRequest, httpServletResponse);
     }
 }
